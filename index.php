9d28083ad3c8640e00b51562c481440b27d4bfc3
AnaAlexandraMatos <1221035@isep.ipp.pt>
2024-05-28 15:55:42 +0100

    commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <title> LSIS1 </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="left-container">
+            <a href="index.php">
+                <img src="https://cdn.icon-icons.com/icons2/787/PNG/512/home_icon-icons.com_64840.png" alt="Ícone da casa" style="width: 32px; height: 32px;">
+            </a>
+            <div class="menuLeft">
+                <a href="#">Sobre nós</a>
+                <ul class="dropdown-menu">
+                    <li><a href="Objetivo.php">O nosso objetivo</a></li>
+                    <li><a href="Nos.php">Quem somos</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="right-container">
+            <div class="menuRight">
+                <a href="index.php">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="Ícone do user" style="width: 32px; height: 32px;">
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="Login.php">Login</a></li>
+                    <li><a href="Register.php">Registo</a></li>
+                    <li><a href="User.php">User</a></li>
+                    <li><a href="Logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+
+</body>
+
+</html>
