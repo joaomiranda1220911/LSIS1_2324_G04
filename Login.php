c3c85e21d0db31a5fca4c16bbcbf4cac1462777b
João Miranda <1220911@isep.ipp.pt>
2024-05-31 16:48:55 +0100

    login

--- Login.php
+++ Login.php
@@ -2,35 +2,58 @@
 <html lang="pt">
 
 <head>
-    <title> LSIS1 </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <title>Página de Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
+        <div class="logo" onclick="window.location.href='index.php'">
+            <img src="Imagens/casa_icon.png" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Sobre Nós</a></li>
+                <li><a href="Dados.php">Dados</a></li>
+                <li><a href="#">Análise</a></li>
+            </ul>
+        </nav>
+        <div class="search-bar">
+            <button class="eredes_btn" onclick="window.open('https://www.e-redes.pt/pt-pt', '_blank');">Site oficial E-redes</button>
+            <input type="text" placeholder="Pesquisar">
+            <button class="search-button">Ir</button>
+        </div>
+        <div class="dropdown">
+            <div class="user-info">
+                <img src="Imagens/user_icon.png" alt="User Icon">
+                <span>Name</span>
+                <div class="dropdown-content">
+                    <a href="Login.php">Login</a>
+                    <a href="#">Registo</a>
+                    <a href="#">Perfil</a>
+                    <a href="#">Sair</a>
+                </div>
+            </div>
 
     </header>
 
-
-
-
-    <form action="login.php" method="POST" class="login-form-container">
-        <div>
-            <h2> Login </h2>
+    <main class="login-container">
+        <div class="login-box">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Palavra-passe">
+            <button>Iniciar Sessão</button>
         </div>
-        <div class="login-form-container">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div class="login-form-container">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button name="submit" type="submit">Login</button>
-    </form>
+        <p>Se ainda não tem conta, crie aqui e comece a tirar partido.</p>
+    </main>
 
+    <footer>
+        <div class="footer-content">
+            <img src="Imagens/isep_logo.png" alt="ISEP Logo">
+            <p>Projeto realizado no âmbito de Laboratório de Sistemas 1</p>
+        </div>
+    </footer>
 </body>
 
 </html>
