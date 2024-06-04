53949eae7b47fd0c1172c8ddd291a98e582d2b5e
João Miranda <1220911@isep.ipp.pt>
2024-06-04 23:46:30 +0100

    favicon

--- SobreNos.php
+++ SobreNos.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Login</title>
+    <title>Sobre Nós</title>
+    <link rel="icon" href="Imagens/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="styles.css">
 
     <style>
